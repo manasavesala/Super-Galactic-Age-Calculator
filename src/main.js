87b865ffdecd068eccd0f534435bcb2c765a6054
Manasa Vesala <vesalamanasa@gmail.com>
2019-01-13 01:31:38 -0800

    Added front-end dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { ageCalculator } from './ageCalculator.js';
 import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   $('#form').submit(function(event) {
